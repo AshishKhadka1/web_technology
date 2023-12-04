8045800f8d29039cbddd882112edcc36975bff73
Ashish khadka <khadkaashish50@gmail.com>
2023-12-04 08:00:02 +0545

    latest

---
+++ practice/script.js
@@ -0,0 +1,25 @@
+// 1.
+let x=10,y=10
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+
+// 2.
+console.log(x>y)
+console.log(x<y)
+console.log(x=y)
+
+// 3.
+let u=0,v=1
+
+let w=u&&v
+console.log(w)
+let a=u||v
+console.log(a)
+let b = u|v
+console.log(b)
+
+// 4.
+
+
