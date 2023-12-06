f2475a416330a544d83eab30dac7996f1d1c73e3
Ashish khadka <khadkaashish50@gmail.com>
2023-12-06 09:29:16 +0545

    newFiles

---
+++ code with harry js/tutorial.js
@@ -0,0 +1,27 @@
+// object declare
+let obj = {name:"john" , age :20}
+obj.age = 33
+console.log(obj)
+
+// if else
+let a = 10
+if (a > 5) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+// switch statement
+let x = "apple"
+switch (x) {
+    case "apple":
+        console.log("x is apple")
+        break;
+
+    case "banana":
+        console.log("x is banana")
+        break;
+    case "default":
+        console.log("x is invalid")
+        }
+
