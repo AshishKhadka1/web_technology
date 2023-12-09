0193265df47e182a8d8012b8356fab32800d8c45
Ashish khadka <khadkaashish50@gmail.com>
2023-12-09 19:48:50 +0545

    modified

---
+++ code with harry js/function.js
@@ -0,0 +1,15 @@
+// square
+function square(x){
+    return x * x * x
+}
+let result = square(5)
+console.log(result)
+
+// add
+function add(x, y){
+    return x + y
+}
+let answer = add(4, 5)
+console.log(answer)
+
+//
