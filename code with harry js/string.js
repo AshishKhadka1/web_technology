89c1d254c0d4ca657d147aca88be9c8b534d9b3c
Ashish khadka <khadkaashish50@gmail.com>
2023-12-11 21:30:01 +0545

    added

--- code with harry js/string.js
+++ code with harry js/string.js
@@ -1,1 +1,15 @@
 // "hello world" 'Hello World' are valid strings
+
+
+// length determination
+var str = "hello boy"
+console.log(str.length)
+
+// combine two or more strings
+var str1 = "ashish"
+var str2 = " khadka"
+console.log(str1.concat(str2))
+
+//indexof used to find the text is in which place(number)
+var str1 = "ramesh yadav"
+console.log(str1.indexOf("y"))
